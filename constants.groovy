159249d0c69c53b49d880aad62420514f7d68935
alfonsojimenezgalan <60976586+alfonsojimenezgalan@users.noreply.github.com>
2021-10-08 20:09:32 +0200

    Create constants.groovy

---
+++ constants.groovy
@@ -0,0 +1,10 @@
+import groovy.transform.Field
+
+@Field
+def testFilesList = 'test-package.xml'
+@Field
+def testdataFilesList = 'TestData.xml'
+@Field
+def gitId = '9ddfsfc4-fdfdf-sdsd-bd18-fgdgdgdf'
+
+return this;
